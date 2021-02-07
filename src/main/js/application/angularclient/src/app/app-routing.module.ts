fbb184002646eebf80f01285f08680036d741b96
VovaProtsiv <vovapro111@gmail.com>
2021-02-07 20:44:56 +0200

    update app-routing.module.ts

--- src/main/js/application/angularclient/src/app/app-routing.module.ts
+++ src/main/js/application/angularclient/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserListComponent } from './user/user-list/user-list.component';
 import { UserFormComponent } from './user/user-form/user-form.component';
 import {EditUserComponent} from "./user/edit-user/edit-user.component";
+import {HotelListComponent} from "./hotel/hotel-list/hotel-list.component";
 
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
   { path: 'adduser', component: UserFormComponent },
   { path: 'edit-user', component: EditUserComponent },
+  { path: 'hotels', component: HotelListComponent }
 ];
 
 @NgModule({
