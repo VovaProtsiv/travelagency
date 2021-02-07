9f8d0e5f4c5d616e4ac273717170cc4b12bc5f9a
VovaProtsiv <vovapro111@gmail.com>
2021-02-07 13:29:31 +0200

    Added NullEntityReferenceException.java

---
+++ src/main/java/dev/pprotsiv/travel/exception/NullEntityReferenceException.java
@@ -0,0 +1,10 @@
+package dev.pprotsiv.travel.exception;
+
+public class NullEntityReferenceException extends RuntimeException{
+    public NullEntityReferenceException() {
+    }
+
+    public NullEntityReferenceException(String message) {
+        super(message);
+    }
+}
