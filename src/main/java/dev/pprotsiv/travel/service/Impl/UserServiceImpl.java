a3297505f3bfa8457299fc58e71949db6f8c5ce2
VovaProtsiv <vovapro111@gmail.com>
2021-02-07 13:29:52 +0200

    Added UserService

---
+++ src/main/java/dev/pprotsiv/travel/service/Impl/UserServiceImpl.java
@@ -0,0 +1,54 @@
+package dev.pprotsiv.travel.service.Impl;
+
+import dev.pprotsiv.travel.exception.NullEntityReferenceException;
+import dev.pprotsiv.travel.model.User;
+import dev.pprotsiv.travel.repo.UserRepository;
+import dev.pprotsiv.travel.service.UserService;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User create(User role) {
+        if (role != null) {
+            return userRepository.save(role);
+        }
+        throw new NullEntityReferenceException("User cannot be 'null'");
+    }
+
+    @Override
+    public User readById(long id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("User with id " + id + " not found"));
+    }
+
+    @Override
+    public User update(User user) {
+        if (user != null) {
+            readById(user.getId());
+            return userRepository.save(user);
+        }
+        throw new NullEntityReferenceException("User cannot be 'null'");
+    }
+
+    @Override
+    public void delete(long id) {
+        userRepository.delete(readById(id));
+    }
+
+    @Override
+    public List<User> getAll() {
+        List<User> users = userRepository.findAll();
+        return users.isEmpty() ? new ArrayList<>() : users;
+    }
+
+
+}
