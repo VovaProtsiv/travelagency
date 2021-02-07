c626a8748a4d3a8ef6f54a377fa862e4cb0cc143
VovaProtsiv <vovapro111@gmail.com>
2021-02-07 20:14:23 +0200

    added hotel-list

---
+++ src/main/js/application/angularclient/src/app/hotel-list/hotel-list.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {Hotel} from "../model/hotel";
+import {ActivatedRoute, Router} from "@angular/router";
+import {UserService} from "../service/user.service";
+import {HotelService} from "../service/hotel-service";
+
+@Component({
+  selector: 'app-hotel-list',
+  templateUrl: './hotel-list.component.html',
+  styleUrls: ['./hotel-list.component.css']
+})
+export class HotelListComponent implements OnInit {
+  hotels: Hotel[];
+
+  constructor(private route: ActivatedRoute, private router: Router, private hotelService: HotelService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
