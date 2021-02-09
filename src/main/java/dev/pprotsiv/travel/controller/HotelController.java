b1249574c5787f4d9c8f22114c57b67be40453c6
VovaProtsiv <vovapro111@gmail.com>
2021-02-09 17:48:15 +0200

    Updated HotelController

--- src/main/java/dev/pprotsiv/travel/controller/HotelController.java
+++ src/main/java/dev/pprotsiv/travel/controller/HotelController.java
@@ -1,6 +1,8 @@
 package dev.pprotsiv.travel.controller;
 
+import dev.pprotsiv.travel.model.Address;
 import dev.pprotsiv.travel.model.Hotel;
+import dev.pprotsiv.travel.service.AddressService;
 import dev.pprotsiv.travel.service.HotelService;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,15 +13,18 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 public class HotelController {
     private final HotelService hotelService;
+    private final AddressService addressService;
 
-    public HotelController(HotelService hotelService) {
+    public HotelController(HotelService hotelService, AddressService addressService) {
         this.hotelService = hotelService;
+        this.addressService = addressService;
     }
 
     @GetMapping("/all")
     public List<Hotel> getHotels() {
-     return  hotelService.getAll();
+        return hotelService.getAll();
     }
+
     @GetMapping("/{id}")
     public Hotel getUserById(@PathVariable long id) {
         return hotelService.readById(id);
@@ -39,4 +44,20 @@ public class HotelController {
     public Hotel editHotel(@RequestBody Hotel hotel) {
         return hotelService.update(hotel);
     }
+
+    @PostMapping("/{hotel_id}/add/address")
+    public Hotel addAddress(@PathVariable long hotel_id, @RequestBody Address address) {
+        Hotel hotel = hotelService.readById(hotel_id);
+        hotel.setAddress(address);
+        return hotelService.update(hotel);
+    }
+
+    @PutMapping("/{hotel_id}/update/address/{address_id}")
+    public Hotel editAddress(@PathVariable long hotel_id, @PathVariable long address_id, @RequestBody Address address) {
+        Hotel hotel = hotelService.readById(hotel_id);
+        addressService.readById(address_id);
+        addressService.update(address);
+        return hotel;
+    }
 }
+
