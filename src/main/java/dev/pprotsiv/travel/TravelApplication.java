2f41c00c45b04b691d84b106083883219cd7494f
VovaProtsiv <vovapro111@gmail.com>
2021-02-09 22:39:21 +0200

    updated TravelApplication.java

--- src/main/java/dev/pprotsiv/travel/TravelApplication.java
+++ src/main/java/dev/pprotsiv/travel/TravelApplication.java
@@ -22,31 +22,4 @@ public class TravelApplication {
         SpringApplication.run(TravelApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner init(UserRepository userRepository, HotelService hotelService, AddressService addressService) {
-        return args -> {
-            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
-                User user = new User(name, name.toLowerCase() + "@domain.com");
-                userRepository.save(user);
-            });
-            /*Stream.of("Quick Stop Hotel","Hillside Hotel","Malibu").forEach(name->{
-                Hotel hotel = new Hotel();
-                hotel.setName(name);
-                hotelService.create(hotel);
-
-            });
-            userRepository.findAll().forEach(System.out::println);
-            System.out.println("-------Hotels-------");
-            hotelService.getAll().forEach(System.out::println);
-            Address address1 = new Address();
-            address1.setCountry("UK1");
-            address1.setCity("IF1");
-            address1.setStreet("Franka1");
-            address1.setHouseNumber("12-b1");
-            address1.setHotel(hotelService.readById(2));
-            addressService.create(address1);
-            System.out.println("------Address-------");
-            addressService.getAll().forEach(System.out::println);*/
-        };
-    }
 }
