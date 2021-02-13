1544853087cc0b56ceafb7e9980b8ca34df7b646
VovaProtsiv <vovapro111@gmail.com>
2021-02-13 19:44:14 +0200

    updated HotelService.java and HotelServiceImpl.java

--- src/main/java/dev/pprotsiv/travel/service/HotelService.java
+++ src/main/java/dev/pprotsiv/travel/service/HotelService.java
@@ -1,13 +1,15 @@
 package dev.pprotsiv.travel.service;
 
 import dev.pprotsiv.travel.model.Hotel;
+import dev.pprotsiv.travel.projection.HotelProjection;
 
 import java.util.List;
 
 public interface HotelService {
     Hotel create(Hotel hotel);
     Hotel readById(long id);
+    HotelProjection readProjectionById(long id);
     Hotel update(Hotel hotel);
     void delete(long id);
-    List<Hotel> getAll();
+    List<HotelProjection> getAllProjections();
 }
