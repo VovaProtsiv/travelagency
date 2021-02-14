d2609962f24a7a05032d02fb93371bdf4997fdc4
VovaProtsiv <vovapro111@gmail.com>
2021-02-14 20:21:19 +0200

    updated RoomController.java

--- src/main/java/dev/pprotsiv/travel/controller/RoomController.java
+++ src/main/java/dev/pprotsiv/travel/controller/RoomController.java
@@ -2,14 +2,17 @@ package dev.pprotsiv.travel.controller;
 
 import dev.pprotsiv.travel.model.Hotel;
 import dev.pprotsiv.travel.model.Room;
+import dev.pprotsiv.travel.projection.RoomProjection;
 import dev.pprotsiv.travel.service.HotelService;
 import dev.pprotsiv.travel.service.RoomService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/hotel")
+@RequestMapping("/rooms")
 @CrossOrigin(origins = "http://localhost:4200")
 public class RoomController {
     private final RoomService roomService;
@@ -21,23 +24,23 @@ public class RoomController {
     }
 
     @GetMapping("/{id}/all")
-    public List<Room> getRoomsByHotelId(@PathVariable long id) {
-        return roomService.getAllByHotelId(id);
+    public ResponseEntity<List<RoomProjection>> getRoomsByHotelId(@PathVariable long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(roomService.getAllProjectionsByHotelId(id));
     }
 
-    @GetMapping("/{hotel_id}/room/{room_id}")
-    public Room getRoom(@PathVariable long hotel_id, @PathVariable long room_id) {
-        return roomService.readById(room_id);
+    @GetMapping("/{room_id}")
+    public ResponseEntity<RoomProjection> getRoom(@PathVariable long room_id) {
+        return ResponseEntity.status(HttpStatus.OK).body(roomService.readProjectionById(room_id));
     }
 
     @PostMapping("/{id}/add")
-    public Room addRoom(@PathVariable long id, @RequestBody Room room) {
+    public ResponseEntity<Room> addRoom(@PathVariable long id, @RequestBody Room room) {
         Hotel hotel = hotelService.readById(id);
         room.setHotel(hotel);
         roomService.create(room);
         hotel.addRoom(room);
         hotelService.update(hotel);
-        return room;
+        return ResponseEntity.status(HttpStatus.OK).body(room);
     }
 
     @PutMapping("/{hotel_id}/edit/{room_id}")
