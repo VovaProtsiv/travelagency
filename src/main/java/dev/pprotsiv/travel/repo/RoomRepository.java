4ae220db77fef8a6e5eb1d3c72867386603e642a
VovaProtsiv <vovapro111@gmail.com>
2021-02-19 19:58:57 +0200

    updated RoomRepository.java

--- src/main/java/dev/pprotsiv/travel/repo/RoomRepository.java
+++ src/main/java/dev/pprotsiv/travel/repo/RoomRepository.java
@@ -9,11 +9,14 @@ import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
-    List<Room> findAllByHotel_Id(long id);
-
     @Query("SELECT r.id as id, r.name as name, r.sleeps as sleeps, r.hotel.id as hotelId FROM Room r WHERE id = ?1")
     RoomProjection findProjectionById(long id);
 
     @Query("SELECT r.id as id, r.name as name, r.sleeps as sleeps, r.hotel.id as hotelId FROM Room r WHERE r.hotel.id = ?1")
     List<RoomProjection> findAllProjectionByHotelId(long id);
+
+    @Query(value = "SELECT r.id as id, r.name as name, r.sleeps as sleeps, r.hotel_id as hotelId " +
+            "FROM rooms r INNER JOIN order_room o ON r.id = o.fk_room " +
+            "WHERE o.fk_order = ?1",nativeQuery = true)
+    List<RoomProjection> findProjectionsByOrderId(long id);
 }
