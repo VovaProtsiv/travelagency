56345773adb0c7099bf1a675f6aa83dff3270a1b
VovaProtsiv <vovapro111@gmail.com>
2021-02-19 19:59:52 +0200

    updated UserController.java

--- src/main/java/dev/pprotsiv/travel/controller/UserController.java
+++ src/main/java/dev/pprotsiv/travel/controller/UserController.java
@@ -1,7 +1,9 @@
 package dev.pprotsiv.travel.controller;
 
 import dev.pprotsiv.travel.model.User;
+import dev.pprotsiv.travel.projection.OrderProjection;
 import dev.pprotsiv.travel.projection.UserProjection;
+import dev.pprotsiv.travel.service.OrderService;
 import dev.pprotsiv.travel.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,38 +14,46 @@ import java.util.List;
 
 
 @RestController
+@RequestMapping("/users")
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     private final UserService userService;
+    private final OrderService orderService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, OrderService orderService) {
         this.userService = userService;
+        this.orderService = orderService;
+
     }
 
-
-    @GetMapping("/users")
+    @GetMapping
     public ResponseEntity<List<UserProjection>> getUsers() {
         return ResponseEntity.status(HttpStatus.OK).body(userService.getAllProjections());
     }
 
-    @GetMapping("/users/{id}")
+    @GetMapping("/{id}/orders")
+    public ResponseEntity<List<OrderProjection>> getOrders(@PathVariable long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(orderService.getProjectionsByUserId(id));
+    }
+
+    @GetMapping("/{id}")
     public ResponseEntity<UserProjection> getUserById(@PathVariable long id) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.getProjection(id));
     }
 
-    @PostMapping("/users")
+    @PostMapping
     public ResponseEntity<User> addUser(@RequestBody User user) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.create(user));
     }
 
-    @DeleteMapping("/users/{id}")
+    @DeleteMapping("{id}")
     public void deleteUser(@PathVariable long id) {
         userService.delete(id);
     }
 
-    @PutMapping("/users/{id}")
+    @PutMapping("{id}")
     public ResponseEntity<User> editUser(@RequestBody User user, @PathVariable long id) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.update(user));
     }
