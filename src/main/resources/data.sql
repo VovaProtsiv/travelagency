29ef9c01c97b76264c7958a8c7038e81e1fd2f16
VovaProtsiv <vovapro111@gmail.com>
2021-02-20 14:16:54 +0200

    updated data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,10 +17,10 @@ insert into rooms (id, hotel_id, name, sleeps) values (1003,1001,'DeLux',2);
 insert into rooms (id, hotel_id, name, sleeps) values (1004,1001,'Lux',2);
 insert into rooms (id, hotel_id, name, sleeps) values (1005,1000,'Single',1);
 
-insert into orders (id, check_in, check_out, client_id, state) values (1000, '2021-08-16','2021-08-17',1000, 'NEW');
-insert into orders (id, check_in, check_out, client_id, state) values (1001, '2021-08-19','2021-08-22',1000,'NEW');
-insert into orders (id, check_in, check_out, client_id, state) values (1002, '2021-08-16','2021-09-28',1002,'DONE');
-insert into orders (id, check_in, check_out, client_id, state) values (1003, '2021-09-01','2021-09-02',1002,'CANCELED');
+insert into orders (id, check_in, check_out, client_id,hotel_id, state) values (1000, '2021-08-16','2021-08-17',1000,1000, 'NEW');
+insert into orders (id, check_in, check_out, client_id,hotel_id, state) values (1001, '2021-08-19','2021-08-22',1000,1000, 'NEW');
+insert into orders (id, check_in, check_out, client_id,hotel_id, state) values (1002, '2021-08-16','2021-09-28',1002,1000, 'DONE');
+insert into orders (id, check_in, check_out, client_id,hotel_id, state) values (1003, '2021-09-01','2021-09-02',1002,1001, 'CANCELED');
 
 insert into order_room (fk_order, fk_room) values (1000, 1005);
 insert into order_room (fk_order, fk_room) values (1001, 1005);
