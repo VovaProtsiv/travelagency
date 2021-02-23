7390460c791f2105b47af2051f440d0d29926ee3
VovaProtsiv <vovapro111@gmail.com>
2021-02-23 20:56:43 +0200

    update UserDetailsImpl.java

--- src/main/java/dev/pprotsiv/travel/security/service/UserDetailsImpl.java
+++ src/main/java/dev/pprotsiv/travel/security/service/UserDetailsImpl.java
@@ -43,7 +43,7 @@ public class UserDetailsImpl implements UserDetails {
 
         return new UserDetailsImpl(
                 user.getId(),
-                user.getName(),
+                user.getUsername(),
                 user.getEmail(),
                 user.getPassword(),
                 authorities);
