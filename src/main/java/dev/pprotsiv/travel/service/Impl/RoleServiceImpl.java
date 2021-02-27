1921b35d775fe8e7da616a64b7bb60cbf70b35ed
VovaProtsiv <vovapro111@gmail.com>
2021-02-27 17:09:28 +0200

    updated RoleService

--- src/main/java/dev/pprotsiv/travel/service/Impl/RoleServiceImpl.java
+++ src/main/java/dev/pprotsiv/travel/service/Impl/RoleServiceImpl.java
@@ -2,9 +2,13 @@ package dev.pprotsiv.travel.service.Impl;
 
 import dev.pprotsiv.travel.model.ERole;
 import dev.pprotsiv.travel.model.Role;
+import dev.pprotsiv.travel.projection.RoleProjection;
 import dev.pprotsiv.travel.repo.RoleRepository;
 import dev.pprotsiv.travel.service.RoleService;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RoleServiceImpl implements RoleService {
@@ -19,5 +23,17 @@ public class RoleServiceImpl implements RoleService {
     public Role findByName(ERole name) {
         return roleRepository.findByName(name)
                 .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+    }
+
+    @Override
+    public List<RoleProjection> findAll() {
+        List<RoleProjection> all = roleRepository.findAllProjections();
+        return all.isEmpty() ? new ArrayList<>() : all;
+    }
+
+    @Override
+    public List<RoleProjection> findByUserId(long id) {
+        List<RoleProjection> all = roleRepository.findByProjectionByUserID(id);
+        return all.isEmpty() ? new ArrayList<>() : all;
     }
 }
