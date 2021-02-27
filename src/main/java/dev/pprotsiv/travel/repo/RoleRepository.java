973d83d6c2f0f0c9970a730ce2874026fa1de509
VovaProtsiv <vovapro111@gmail.com>
2021-02-27 17:08:48 +0200

    updated RoleRepoitory.java

--- src/main/java/dev/pprotsiv/travel/repo/RoleRepository.java
+++ src/main/java/dev/pprotsiv/travel/repo/RoleRepository.java
@@ -2,12 +2,21 @@ package dev.pprotsiv.travel.repo;
 
 import dev.pprotsiv.travel.model.ERole;
 import dev.pprotsiv.travel.model.Role;
+import dev.pprotsiv.travel.projection.RoleProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(ERole name);
+
+    @Query("SELECT name as name FROM Role")
+    List<RoleProjection> findAllProjections();
+
+    @Query(value = "SELECT r.name as name FROM roles r INNER JOIN user_roles u ON u.role_id = r.id AND u.user_id = ?1",nativeQuery = true)
+    List<RoleProjection> findByProjectionByUserID(long id);
 }
