75bd125abb79dbec0369631a9d20c5e135a4fe1d
VovaProtsiv <vovapro111@gmail.com>
2021-03-02 20:35:50 +0200

    updated OrderController.java

--- src/main/java/dev/pprotsiv/travel/controller/OrderController.java
+++ src/main/java/dev/pprotsiv/travel/controller/OrderController.java
@@ -1,10 +1,12 @@
 package dev.pprotsiv.travel.controller;
 
+import dev.pprotsiv.travel.dto.OrderDto;
 import dev.pprotsiv.travel.model.Order;
 import dev.pprotsiv.travel.model.User;
 import dev.pprotsiv.travel.projection.OrderProjection;
 import dev.pprotsiv.travel.service.OrderService;
 import dev.pprotsiv.travel.service.UserService;
+import org.springframework.data.domain.jaxb.SpringDataJaxb;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin("http://localhost:4200")
 public class OrderController {
     private final OrderService orderService;
     private final UserService userService;
@@ -28,6 +30,10 @@ public class OrderController {
         return ResponseEntity.status(HttpStatus.OK).body(orderService.getProjectionsByUserId(userId));
     }
 
+    @PostMapping("/{userId}")
+    public ResponseEntity<Order> createOrder(@PathVariable long userId, @RequestBody OrderDto dto){
+        return ResponseEntity.status(HttpStatus.OK).body(orderService.create(dto));
+    }
 
     @DeleteMapping("/remove/{order_id}")
     public void deleteRoom(@PathVariable long order_id) {
