7f3a850be938d709d4c9b833ef8732b6fe8d5921
VovaProtsiv <vovapro111@gmail.com>
2021-03-07 11:12:30 +0200

    updated OrderRepository.java

--- src/main/java/dev/pprotsiv/travel/repo/OrderRepository.java
+++ src/main/java/dev/pprotsiv/travel/repo/OrderRepository.java
@@ -1,6 +1,7 @@
 package dev.pprotsiv.travel.repo;
 
 import dev.pprotsiv.travel.model.Order;
+import dev.pprotsiv.travel.model.State;
 import dev.pprotsiv.travel.projection.OrderProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,8 +12,11 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("SELECT o.id as id, o.checkIn as checkIn, o.checkOut as checkOut, o.client.id as userId," +
             "o.state as state, o.hotel.id as hotelId, o.hotel.name as hotelName FROM Order o WHERE o.client.id =?1")
-    List<OrderProjection> getAllOrderProjectionByUserId(long id);
+    List<OrderProjection> getAllOrderProjectionByUserId(long id );
 
+    @Query("SELECT o.id as id, o.checkIn as checkIn, o.checkOut as checkOut, o.client.id as userId," +
+            "o.state as state, o.hotel.id as hotelId, o.hotel.name as hotelName FROM Order o WHERE o.hotel.id =?1 AND o.state =?2")
+    List<OrderProjection> getAllOrderProjectionByHotelIdAndState(long id, State state);
 
     @Query("SELECT o.id as id, o.checkIn as checkIn, o.checkOut as checkOut, o.hotel.id as hotelId, o.hotel.name as hotelName" +
             " FROM Order o WHERE o.id =?1")
