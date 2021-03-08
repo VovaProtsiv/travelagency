7fae9ffdb8c7cd88ecd3859f81796c028313ac90
VovaProtsiv <vovapro111@gmail.com>
2021-03-09 00:33:02 +0200

    Added RestResponseEntityExceptionHandler.java

---
+++ src/main/java/dev/pprotsiv/travel/exception/handler/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,32 @@
+package dev.pprotsiv.travel.exception.handler;
+
+import dev.pprotsiv.travel.exception.BookedRoomsExceptions;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value
+            = {IllegalArgumentException.class, BookedRoomsExceptions.class})
+    protected ResponseEntity<Object> handleConflict(
+            RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = ex.getMessage();
+        return handleExceptionInternal(ex, bodyOfResponse,
+                new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(value
+            = {RuntimeException.class})
+    protected ResponseEntity<Object> handleRuntimeExc(
+            RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = ex.getMessage();
+        return handleExceptionInternal(ex, bodyOfResponse,
+                new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+}
