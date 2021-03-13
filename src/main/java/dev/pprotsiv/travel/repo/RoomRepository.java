a834380421c04e3c9f56dc33df4ac47206424939
VovaProtsiv <vovapro111@gmail.com>
2021-03-13 20:02:31 +0200

    updated RoomRepository.java

--- src/main/java/dev/pprotsiv/travel/repo/RoomRepository.java
+++ src/main/java/dev/pprotsiv/travel/repo/RoomRepository.java
@@ -12,13 +12,13 @@ import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
-    @Query("SELECT r.id as id, r.name as name, r.sleeps as sleeps, r.hotel.id as hotelId FROM Room r WHERE id = ?1")
+    @Query("SELECT r.id as id, r.name as name, r.sleeps as sleeps, r.hotel.id as hotelId, r.price as price FROM Room r WHERE id = ?1")
     RoomProjection findProjectionById(long id);
 
-    @Query("SELECT r.id as id, r.name as name, r.sleeps as sleeps, r.hotel.id as hotelId FROM Room r WHERE r.hotel.id = ?1")
+    @Query("SELECT r.id as id, r.name as name, r.sleeps as sleeps, r.hotel.id as hotelId, r.price as price FROM Room r WHERE r.hotel.id = ?1")
     List<RoomProjection> findAllProjectionByHotelId(long id);
 
-    @Query(value = "SELECT r.id as id, r.name as name, r.sleeps as sleeps, r.hotel_id as hotelId " +
+    @Query(value = "SELECT r.id as id, r.name as name, r.sleeps as sleeps, r.hotel_id as hotelId, r.price as price " +
             "FROM rooms r INNER JOIN order_room o ON r.id = o.fk_room " +
             "WHERE o.fk_order = ?1", nativeQuery = true)
     List<RoomProjection> findProjectionsByOrderId(long id);
