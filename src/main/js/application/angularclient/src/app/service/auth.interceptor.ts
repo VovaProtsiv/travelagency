9800ed49eeed4d0d9cecd63c947c5872cd31a23b
VovaProtsiv <vovapro111@gmail.com>
2021-03-14 13:02:45 +0200

    handle 401 error

--- src/main/js/application/angularclient/src/app/service/auth.interceptor.ts
+++ src/main/js/application/angularclient/src/app/service/auth.interceptor.ts
@@ -1,16 +1,25 @@
-import {HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest
+} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 
 
 import {Observable} from 'rxjs';
 import {TokenStorage} from "./token-storage";
+import {tap} from "rxjs/operators";
+import {Router} from "@angular/router";
 
 const TOKEN_HEADER_KEY = 'Authorization';       // for Spring Boot back-end
 // const TOKEN_HEADER_KEY = 'x-access-token';   // for Node.js Express back-end
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private token: TokenStorage) {
+  constructor(private token: TokenStorage,private router: Router) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -23,7 +32,15 @@ export class AuthInterceptor implements HttpInterceptor {
       // for Node.js Express back-end
       // authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, token) });
     }
-    return next.handle(authReq);
+    return next.handle(authReq).pipe( tap(() => {},
+      (err: any) => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status !== 401) {
+            return;
+          }
+          this.router.navigate(['login']);
+        }
+      }));
   }
 }
 
