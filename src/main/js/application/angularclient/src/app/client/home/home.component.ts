32b08018941c25069195d11b369977e72be6a71e
VovaProtsiv <vovapro111@gmail.com>
2021-03-14 19:33:58 +0200

    added date validation

--- src/main/js/application/angularclient/src/app/client/home/home.component.ts
+++ src/main/js/application/angularclient/src/app/client/home/home.component.ts
@@ -4,6 +4,7 @@ import {HotelService} from "../../service/hotel-service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {TokenStorage} from "../../service/token-storage";
 import {RoomService} from "../../service/room-service";
+import {FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -13,14 +14,30 @@ import {RoomService} from "../../service/room-service";
 export class HomeComponent implements OnInit {
   text: string = '';
   hotels: Hotel[];
-  checkIn: Date;
-  checkOut: Date;
+  myForm: FormGroup;
+  minDate: any;
+  maxDate: any;
+  isValid: boolean;
 
-  constructor(private hotelService: HotelService, private rout: ActivatedRoute, private route: Router, private tokenStorage: TokenStorage, private roomService: RoomService) {
+  constructor(private hotelService: HotelService, private formBuilder: FormBuilder, private rout: ActivatedRoute, private route: Router, private tokenStorage: TokenStorage, private roomService: RoomService) {
   }
 
   ngOnInit(): void {
     this.hotelService.findAll().subscribe(data => this.hotels = data);
+    let date = new Date();
+    this.minDate = new Date();
+    date.setDate(date.getDate() + 1);
+    this.maxDate = date;
+
+    this.myForm = this.formBuilder.group({
+      'checkIn': ['', Validators.required],
+      'checkOut': ['', Validators.required],
+    });
+    this.myForm.setValue({
+      'checkIn': (new Date()).toISOString().substring(0, 10),
+      'checkOut': (date).toISOString().substring(0, 10)
+    });
+    this.myForm.setValidators(this.comparisonValidator())
   }
 
   onChange(name: string) {
@@ -32,15 +49,30 @@ export class HomeComponent implements OnInit {
     this.route.navigate(['/home/' + this.tokenStorage.getUser().id + '/hotel/' + id],
       {
         relativeTo: this.rout, queryParams: {
-          check_in: this.checkIn,
-          check_out: this.checkOut
+          check_in: this.myForm.value.checkIn,
+          check_out: this.myForm.value.checkOut
         },
       })
   }
 
-
-
   onSubmit() {
+  }
 
+  public comparisonValidator(): ValidatorFn {
+    return (group: FormGroup): ValidationErrors => {
+      const checkIn = group.controls['checkIn'];
+      const checkOut = group.controls['checkOut'];
+      let date = new Date();
+      date.setDate(date.getDate());
+
+      if (checkIn.value >= checkOut.value) {
+        checkOut.setErrors({"Check-out should be greater than check-in": false});
+        this.isValid = false;
+      } else {
+        checkOut.setErrors(null);
+        this.isValid = true;
+      }
+      return;
+    };
   }
 }
