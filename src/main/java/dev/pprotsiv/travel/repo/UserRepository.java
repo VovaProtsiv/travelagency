efe1343bc55ed7c1cdf9495e7027743e9aa1895b
VovaProtsiv <vovapro111@gmail.com>
2021-03-21 11:40:18 +0200

    deleted methods with projections

--- src/main/java/dev/pprotsiv/travel/repo/UserRepository.java
+++ src/main/java/dev/pprotsiv/travel/repo/UserRepository.java
@@ -1,22 +1,13 @@
 package dev.pprotsiv.travel.repo;
 
 import dev.pprotsiv.travel.model.User;
-import dev.pprotsiv.travel.projection.UserProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-
-    @Query("SELECT u.id as id, u.username as name, u.email as email FROM User u")
-    List<UserProjection> getAllProjections();
-
-    @Query("SELECT u.id as id, u.username as name, u.email as email FROM User u WHERE u.id = ?1")
-    UserProjection findProjectionById(long id);
 
     Optional<User> findByUsername(String username);
 
